efa70bfc297a911438c362d06db31443379e7f44
Renata <renatasilva.bsb@gmail.com>
2020-05-18 00:44:14 -0300

    Estruturas de Repetição

---
+++ Aula1/ex01.py
@@ -0,0 +1,56 @@
+#EX.01
+
+
+#formatação de saída
+
+
+
+exit ()
+#and
+print (5>3 and 3<4)
+#or
+print (5>3 and 55<4)
+
+
+exit ()
+#boolean True or False true=!Ture
+
+print (5>3) # 
+print (3>10)
+print (5==5) #comparação de igualdade comper iqual
+print (3!=2)
+print (5>=10)
+print (6<=12)
+
+
+
+exit()
+num1 = 12 
+num2 = 15
+
+print (num1+num2)
+
+num1 = int(input('Digite o primeiro número:'))
+num2 = float (input ('Digite o segundo número:'))
+
+#num1 = input ('Digite o primeiro número:')
+#num2 = input ('Digite o segundo número:')
+
+#print (num1+num2)  #soma de strings significa colocar uma do lado da outra
+#print (num1*2)
+
+
+exit ()
+#python escreva uma saudação que contenha o nome e a idade
+##entrada
+
+# 1 : Python , capture as informações de nome e de idade
+nome = input ('Digite o Seu nome: ')  #imput sempre ira considera um texto string
+idade = input ('Digite a sua idade: ')
+
+# 2: saída
+
+print ('Olá, meu nome é ',nome, 'e tenho ' , idade, 'anos.')
+
+
+
---
+++ Aula1/teste.py
@@ -0,0 +1,18 @@
+#Primeiro arquivo
+
+print ('Ola Mundo')
+
+# fortemente tipado
+
+texto = '4linux'
+numero = 15
+
+#Linguagem Interpretada
+#Interpretador
+#Preço # Cpython 
+#Entrada -> processamento -> saída
+
+##Problemas
+#1.Descrever bem o problema
+##Algoritimos
+
